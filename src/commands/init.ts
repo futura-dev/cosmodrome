6080eca80dc08bd6cb8191833b2c330012362e49
faridevnz <alessio.farinelli.developer@gmail.com>
2023-05-18 17:15:18 +0200

    chore: documentation updated

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,4 +1,20 @@
 import * as fs from 'fs'
+import {Signale, SignaleOptions} from 'signale'
+
+
+const baseSignalOptions: SignaleOptions = {
+  types: {
+    complete: {
+      badge: '**',
+      color: 'cyan',
+      label: 'completed'
+    }
+  },
+  config: {
+    displayScope: false,
+  }
+}
+const s = new Signale({...baseSignalOptions, types: { ...baseSignalOptions.types, newline: { badge: '',label: '', color: 'black' } } })
 
 export const init = (): Promise<void> => {
   fs.writeFileSync('./.cosmodrome.json', JSON.stringify(
@@ -17,7 +33,10 @@ export const init = (): Promise<void> => {
       // eslint-disable-next-line comma-dangle
     }, null, 2)
   )
-  console.log('.cosmodrome.json file was successfully created 🚀')
+  s.complete('.cosmodrome.json file was successfully created')
+  const gitignore = fs.readFileSync('.gitignore', { encoding: 'utf8' })
+  fs.writeFileSync('.gitignore', gitignore + '\n# cosmodrome\n.cosmodrome.json\n', { flag: 'w' });
+  s.complete('added .cosmodrome.json to .gitignore')
   // return
   return Promise.resolve()
 }
